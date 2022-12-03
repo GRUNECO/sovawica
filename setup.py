1a4a2ae8bd1cd2e87977824061ae2bf56e5a84ce
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2022-12-03 17:47:19 -0500

    versioneer

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
 import setuptools
+import versioneer
+
+# Give setuptools a hint to complain if it's too old a version
+# 30.3.0 allows us to put most metadata in setup.cfg
+# 38.3.0 contains most setup.cfg bugfixes
+# Should match pyproject.toml
+SETUP_REQUIRES = ["setuptools >= 38.3.0"]
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="sovawica",
-    version="0.0.1",
     author="Brayan Hoyos Madera,Yorguin Mantilla",
     description="Wavelet ICA",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    install_requires = ['numpy', 'PyWavelets'],
+    install_requires = ['numpy', 'PyWavelets','versioneer'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
 )
